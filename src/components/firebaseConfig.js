addd275ce8ad4620063e514b2dfedc89fce09436
ainpoenya <kurniawan@belidomain.web.id>
2018-05-27 01:26:30 +0700

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default { 
-  apiKey: "AIzaSyC0R5TcGvDOlAPHoSsmIe2VBgGJnpVOnQw",
-    authDomain: "inilab-v2.firebaseapp.com",
-    databaseURL: "https://inilab-v2.firebaseio.com",
-    projectId: "inilab-v2",
-    storageBucket: "inilab-v2.appspot.com",
-    messagingSenderId: "539117552953"
-}
